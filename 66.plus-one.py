8c050d0db0dcc897b0d762f6bb8757701ed443a3
Subhadra Shinde <subhadrashinde0929@gmail.com>
2026-06-17 17:13:17 +0530

    +1 to a array of nums

---
+++ 66.plus-one.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        return [1] + digits
+
+
+        
+# @lc code=end
+
