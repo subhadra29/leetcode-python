33a1259a6de193414d53960e105e47e43407fad3
Subhadra Shinde <subhadrashinde0929@gmail.com>
2026-06-25 11:59:34 +0530

    changing binary tree into inorder

---
+++ 94.binary-tree-inorder-traversal.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output=[]
+        def inorder(root):
+            if not root:
+                return
+            
+            inorder(root.left)
+            output.append(root.val)
+            inorder(root.right)
+
+        
+        inorder(root)
+        return output
+
+        
+        
+    
+        
+        
+
+        
+        
+    
+
+    
+        
+
+
+        
+# @lc code=end
+
